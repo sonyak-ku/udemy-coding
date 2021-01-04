ccc9ebc19aef6b894e276d7214c53bd8b1c84518
sonyak <kch9141@naver.com>
2021-01-04 14:25:00 +0900

    2021-01-04 changed location

---
+++ pract.js
@@ -0,0 +1,6 @@
+
+const first = prompt("what's your first name? sir")
+const last = prompt("what's your last name sir?")
+const age = prompt("How old are you, sir")
+
+console.log("His name is "+first+' '+last+' and he is '+age+' years old.')
